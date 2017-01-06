03b4d973e16e62c0f1e3a8f7d625b9735507eec0
Martin Disch <martindisch@gmail.com>
2017-01-06 21:05:37 +0100

    More useful output

--- readdata.py
+++ readdata.py
@@ -14,14 +14,19 @@ def humConvert(rawTemp, rawHum):
     hum = (float(rawHum) / 65536) * 100
     return [temp, hum]
 
+MAC = '24:71:89:BC:84:84'
 adapter = pygatt.GATTToolBackend()
 
 try:
     adapter.start()
-    device = adapter.connect('24:71:89:BC:84:84')
+    print "Connecting to " + MAC
+    device = adapter.connect(MAC)
+    print "Connected. Enabling sensors"
     device.char_write('f000aa02-0451-4000-b000-000000000000', bytearray([0x01]))
     device.char_write('f000aa22-0451-4000-b000-000000000000', bytearray([0x01]))
+    print "Sensors enabled. Starting measurements"
     time.sleep(2)
+    print "\n"
     while True:
         valueTemp = device.char_read('f000aa01-0451-4000-b000-000000000000')
         valueHum = device.char_read('f000aa21-0451-4000-b000-000000000000')
@@ -35,6 +40,7 @@ try:
         rawTemp = int ('0x' + bytes[1] + bytes[0], 16)
         rawHum = int ('0x' + bytes[3] + bytes[2], 16)
         resultsHum = humConvert(rawTemp, rawHum)
+        print "\033[3A"
         print "Obj: {:.1f} Amb: {:.1f}".format(resultsTemp[0], resultsTemp[1])
         print "Tmp: {:.1f} Hum: {:.1f}".format(resultsHum[0], resultsHum[1])
         time.sleep(1)
