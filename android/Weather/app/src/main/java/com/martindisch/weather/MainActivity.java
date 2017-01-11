d3ef058102391e887a3e8697f948a446c1269190
Martin Disch <martindisch@gmail.com>
2017-01-11 10:57:34 +0100

    Use SwipeRefreshLayout

--- android/Weather/app/src/main/java/com/martindisch/weather/MainActivity.java
+++ android/Weather/app/src/main/java/com/martindisch/weather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.martindisch.weather;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +21,7 @@ import cz.msebera.android.httpclient.Header;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mLatestTemp, mLatestHum, mHistory;
+    private SwipeRefreshLayout mSwipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +31,68 @@ public class MainActivity extends AppCompatActivity {
         mLatestTemp = (TextView) findViewById(R.id.tvLatestTemp);
         mLatestHum = (TextView) findViewById(R.id.tvLatestHum);
         mHistory = (TextView) findViewById(R.id.tvHistory);
+        mSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
 
+        mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchData();
+            }
+        });
+
+        fetchData();
+    }
+
+    private void fetchData() {
+        mSwipeContainer.setRefreshing(true);
         AsyncHttpClient client = new AsyncHttpClient();
         client.setMaxRetriesAndTimeout(1, 500);
         client.get("http://" + getString(R.string.IP) + "/history", new AsyncHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                try {
-                    ArrayList<String[]> history = Util.parseHistory(responseBody);
-                    String[] latest = history.get(history.size() - 1);
-                    mLatestTemp.setText(String.format(getString(R.string.format_temp), latest[1]));
-                    mLatestHum.setText(String.format(getString(R.string.format_hum), latest[2]));
-                    String history_out = "";
-                    for (String[] current : history) {
-                        history_out += current[0] + ":" + String.format(getString(R.string.format_both), current[1], current[2]) + "\n";
+            public void onSuccess(int statusCode, Header[] headers, final byte[] responseBody) {
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            ArrayList<String[]> history = Util.parseHistory(responseBody);
+                            final String[] latest = history.get(history.size() - 1);
+                            String history_out = "";
+                            for (String[] current : history) {
+                                history_out += current[0] + ":" + String.format(getString(R.string.format_both), current[1], current[2]) + "\n";
+                            }
+                            final String h = history_out;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mLatestTemp.setText(String.format(getString(R.string.format_temp), latest[1]));
+                                    mLatestHum.setText(String.format(getString(R.string.format_hum), latest[2]));
+                                    mHistory.setText(h);
+                                }
+                            });
+                        } catch (IOException e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), getString(R.string.error_parsing), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } finally {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mSwipeContainer.setRefreshing(false);
+                                }
+                            });
+                        }
                     }
-                    mHistory.setText(history_out);
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(), getString(R.string.error_parsing), Toast.LENGTH_SHORT).show();
-                }
+                }).start();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Toast.makeText(getApplicationContext(), getString(R.string.error_connecting), Toast.LENGTH_SHORT).show();
+                mSwipeContainer.setRefreshing(false);
             }
         });
     }
